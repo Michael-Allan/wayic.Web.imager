0a2963506e16587a539d78242846d5b64d9407fe
Michael Allan <mike@reluk.ca>
2020-10-29 12:19:32 -0400

    Dummy implementation of `image` command.

---
+++ BuildTarget.java
@@ -0,0 +1,24 @@
+package wayic.Web.imager;
+
+// Changes to this file immediately affect the next runtime.  Treat it as a script.
+
+
+/** A build target of the present project.  Shell commands will accept abbreviated target names:
+  * a target may be specified by any substring of its name that matches no other target name.
+  */
+public enum BuildTarget {
+
+
+    /** A software builder compiled from source code into Java class files.
+      * All other targets depend on this target and include it implicitly.
+      */
+    builder, // Mandatory, do not remove.
+
+
+    /** Java class files compiled from source code.
+      */
+    Java_class_files; }
+
+
+
+                                                        // Copyright © 2020  Michael Allan.  Licence MIT.
