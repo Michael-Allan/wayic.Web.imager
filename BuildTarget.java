b4820219a4b756b3148515d290047ff2a228b1b5
Michael Allan <mike@reluk.ca>
2020-11-02 12:47:43 -0500

    Clarity.

--- BuildTarget.java
+++ BuildTarget.java
@@ -1,6 +1,6 @@
 package wayic.Web.imager;
 
-// Changes to this file immediately affect the next runtime.  Treat it as a script.
+// Changes to this file immediately affect the next build.  Treat it as a build script.
 
 
 /** A build target of the present project.  Shell commands will accept abbreviated target names:
