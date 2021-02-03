10da2d12094b2a737772b60aebbede03d9544f72
Michael Allan <mike@reluk.ca>
2021-02-03 17:53:09 -0500

    Sync with base imager.

--- WaybrecHTMLTransformer.java
+++ WaybrecHTMLTransformer.java
@@ -1,8 +1,11 @@
 package wayic.Web.imager;
 
-import Breccia.parser.ParseError;
+import Breccia.parser.*;
 import Breccia.Web.imager.FileTransformer;
-import java.io.*;
+import Breccia.Web.imager.TransformError;
+import java.io.IOException;
+import java.io.Reader;
+import java.net.URI;
 import java.nio.file.Path;
 import Java.Unhandled;
 import javax.xml.stream.XMLStreamException;
@@ -10,52 +13,67 @@ import wayic.Waybrec.parser.WaybrecCursor;
 import wayic.Waybrec.parser.WaybrecXCursor;
 
 import static Breccia.parser.BrecciaXCursor.EMPTY;
-import static Breccia.parser.BrecciaXCursor.START_DOCUMENT;
-import static java.nio.charset.StandardCharsets.UTF_8;
+import static Breccia.parser.Project.newSourceReader;
+import static Breccia.Web.imager.Imaging.imageSimpleName;
 import static java.nio.file.Files.createFile;
-import static java.nio.file.Files.newInputStream;
 import static wayic.Web.imager.Project.logger;
 
 
-public final class WaybrecHTMLTransformer implements FileTransformer {
+public final class WaybrecHTMLTransformer implements FileTransformer<WaybrecCursor> {
 
 
-    /** @param plainTransformer The transformer to use for non-waycast files.
+    /** @see #inX
+      * @param plainTransformer The transformer to use for non-waycast files.
       */
-    public WaybrecHTMLTransformer( final FileTransformer plainTransformer ) {
+    public WaybrecHTMLTransformer( WaybrecXCursor inX,
+          FileTransformer<BrecciaCursor> plainTransformer ) {
+        this.inX = inX;
         this.plainTransformer = plainTransformer; }
+
+
+
+    /** The source translator to use during calls to this transformer for waycast files.
+      * Between calls, it may be used for other purposes.
+      */
+    public final WaybrecXCursor inX;
 
 
 
    // ━━━  F i l e   T r a n s f o r m e r  ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 
+    public @Override Markup formalReferenceAt( final WaybrecCursor sourceCursor ) {
+        Markup markup = plainTransformer.formalReferenceAt( sourceCursor );
+        if( markup == null ) {
+            /* TODO, the Waybreccian part */; }
+        return markup; }
+
+
+
+    public @Override WaybrecCursor sourceCursor() { return inX.sourceCursor(); }
+
+
+
     public @Override void transform( final Path sourceFile, final Path imageDirectory )
-          throws IOException, ParseError {
+          throws ParseError, TransformError {
         if( !isWaycastFile( sourceFile )) {
             plainTransformer.transform( sourceFile, imageDirectory );
             return; }
         // TODO below, share common code with `Breccia.Web.imager.BrecciaHTMLTransformer`.
-        try( final InputStream byteSource = newInputStream​( sourceFile );
-             final InputStreamReader charSource = new InputStreamReader( byteSource, UTF_8 )) {
-               // Cursor `in` does the buffering of `charSource` recommended by `InputStreamReader`.
-               // The underlying `byteSource` needs none.  https://stackoverflow.com/a/27347262/2402790
-            in.markupSource( charSource );
-            final int t = in.getEventType();
-            if( t == EMPTY ) {
-                logger.fine( () -> "Imaging empty source file: " + sourceFile );
-                final Path imageFile = imageDirectory.resolve( sourceFile.getFileName() + ".xht" );
-                createFile( imageFile );
-                return; }
-            assert t == START_DOCUMENT;
+        try( final Reader source = newSourceReader​( sourceFile )) {
+            inX.markupSource( source ); /* Better not to parse functionally using `inX.perState`
+              and mess with shipping a checked `TransformError` out of the lambda function. */
             for( ;; ) {
-                // TODO, the actual transform.
-                if( !in.hasNext() ) break;
-                try { in.next(); }
-                catch( final XMLStreamException x ) {
-                    final var cause = x.getCause();
-                    if( cause instanceof ParseError ) throw (ParseError)cause;
-                    throw new Unhandled( x ); }}}}
+                switch( inX.getEventType() ) {
+                    case EMPTY -> {
+                        logger.fine( () -> "Imaging empty source file: " + sourceFile );
+                        final Path imageFile = imageDirectory.resolve( imageSimpleName( sourceFile ));
+                        createFile( imageFile ); }}
+                    // TODO, the actual transform for other states.
+                if( !inX.hasNext() ) break;
+                try { inX.next(); }
+                catch( final XMLStreamException x ) { throw (ParseError)(x.getCause()); }}}
+        catch( IOException x ) { throw new Unhandled( x ); }}
 
 
 
@@ -68,11 +86,7 @@ public final class WaybrecHTMLTransformer implements FileTransformer {
 
 
 
-    private final FileTransformer plainTransformer;
-
-
-
-    private final WaybrecXCursor in = new WaybrecXCursor( new WaybrecCursor() ); }
+    private final FileTransformer<BrecciaCursor> plainTransformer; }
 
 
 
